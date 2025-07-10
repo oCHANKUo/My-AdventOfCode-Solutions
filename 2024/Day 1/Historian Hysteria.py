c145cff58cc0f5d4a6621d6cd1427647731afaf2
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-07-10 10:06:52 +0530

    Main logic. Ill probably tidy this up and make it more efficient with as few lines as possible

--- 2024/Day 1/Historian Hysteria.py
+++ 2024/Day 1/Historian Hysteria.py
@@ -1,3 +1,5 @@
+#Ultimate Goal: 2 sorted lists. Find the total sum of the differences between elements at each matching indexes.
+
 left_list = []
 right_list = []
 
@@ -5,9 +7,21 @@ print("Enter each of your values seperated by a blank space")
 
 while True:
     user_input = input()
-    if not user_input.strip():
+    if not user_input:
         break
     inputParts = user_input.split()
     left_list.append(int(inputParts[0]))
+    right_list.append(int(inputParts[1]))
 
-print (left_list)
+left_list.sort()
+right_list.sort()
+
+total_sum = 0
+difference = 0
+
+for i in range (len(left_list)):
+    difference = abs(left_list[i] - right_list[i])
+    total_sum = total_sum + difference
+
+
+print(total_sum)
